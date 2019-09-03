2ed68c088a656ed83f28a25d58c4bd31a1911947
shahidir <36573990+shahidir@users.noreply.github.com>
2019-09-03 13:58:42 -0400

    Combining of IGT files for CARI data
    Uses regular IGT data as well as the IDAT info

--- IGT_Combine.R
+++ IGT_Combine.R
@@ -1,6 +1,8 @@
 rm(list=ls())
 library(foreign)
-
+library(stringr)
+#All IGT files must be named as IGT_subjID_Date.txt
+#_ can be -, date can be mm-dd-yy or mm-dd-yyyy or any format with 2 "-"
 
 #setwd( "~/Desktop/Impulsivity" ) #Sets working directory to folder where SPSS file is
 #logData <- read.spss("R01 IMPULSIIVITY DATABASE 4-24-2019.sav",to.data.frame=TRUE, use.value.labels=FALSE) #This is if there is a master database, all code relating to master database highlighted out
@@ -41,190 +43,82 @@ for (subjIdx in 1:numSubjs) {
 
 
 # Combining all IGT data --------------------------------------------------
-groupInfo = logData 
-allFiles = dir()
-numSubjs = length( allFiles )
-allData = NULL
-contDataM = NULL
-contDataF = NULL
-heroinDataM = NULL
-heroinDataF = NULL
-amphDataM = NULL
-amphDataF = NULL
-polyDataM=NULL
-polyDataF=NULL
-sibHeroinM=NULL
-sibHeroinF=NULL
-sibAmphetamineM=NULL
-sibAmphetamineF=NULL
-allM=NULL
-allF=NULL
-
-allFiles = dir()
-numSubjs = length( allFiles )
-for (subjIdx in 1:numSubjs) {
-  #subjIdx=582
+importFunction1<-function(df){ #This function describes what to do if the IGT file has the IGT description in place
+  fileName<-str_replace_all(tmpFile, fixed("_"), "-") #Replaces all _ with - to maintain consistency in filename
+  dashLocation<-lapply(strsplit(fileName, ''), function(fileName) which(fileName == '-')) #Gets location of every dash in the file name
+  dashLocation<-unlist(dashLocation) #Converts the list of locations into extractable info
+  txtLocation<-lapply(strsplit(fileName, ''), function(fileName) which(fileName == '.')) #Gets location of .txt in file name
+  txtLocation<-unlist(txtLocation) #Converts the list into extractable info
+  
+  if(length(dashLocation)==1){ #This is for subjects who do not have a date in the filename.
+    subjID=substring(fileName, (dashLocation[1]+1), (txtLocation[1]-1)) #Gets the date from filename using period as reference
+    date=NA
+  }else{
+    subjID = substring(fileName, (dashLocation[1]+1),(dashLocation[2]-1))  # gets subject ID from filename using dashes as references
+    date=substring(fileName, (dashLocation[2]+1), (txtLocation[1]-1)) #Gets the date from filename using period as reference  
+  }
   
   
-  tmpFile = allFiles[ subjIdx ]
-  tmp= read.table(tmpFile, skip=21, header=T)
-  if(nrow(tmp)!=100){
-    tmp= read.table(tmpFile, header=T)
-  }
-  if(nrow(tmp)==101 & as.character(tmp[1,1])=="Trial"){
-    tmp<-tmp[2:101,]
-  }
-  if(nrow(tmp)==101 & as.character(tmp[101,1])=="Trial"){
-    tmp<-tmp[1:100,]
-  }
-  
-  tmp2 = tmp[1:100,] #Check this value here if the numbers dont line up, need to match IGT output************************************************************************************************************
-  subjID = substring(tmpFile, 1,3)  # subject ID
-  tmp3 = cbind( tmp2, subjID)  # add a col. with subjID
-  # replace A', B', C', D' with 1, 2, 3, 4
-  deckTmp = tmp3$Deck
-  deckTmp2 = gsub("A", 1, deckTmp)
-  deckTmp2 = gsub("B", 2, deckTmp2)
-  deckTmp2 = gsub("C", 3, deckTmp2)
-  deckTmp2 = gsub("D", 4, deckTmp2)
-  tmp3$Deck = deckTmp2 # now replace abcd with deckTmp2
+  df = cbind( df, subjID)  # add a col. with subjID
+  colnames(df)[which(names(df) == "subjID")] <- "subject" #Renames subjectID column to match other files
+  df = cbind( df, date)  # add a col. with date
+  df$Borrow<-NULL #Removes borrow column since it is not in some of the raw data
+  df$Deck = gsub("A'", 1, df$Deck) #Replaces default values to match other database
+  df$Deck = gsub("B'", 2, df$Deck)
+  df$Deck = gsub("C'", 3, df$Deck)
+  df$Deck = gsub("D'", 4, df$Deck)
   
   
-  tmpLog = subset( groupInfo, PRE_1_SUBJ==subjID)  # find this subject's group membership using PRE_1_SUBJ from SPSS file
-  tmpDx = tmpLog[,"PRE_GROUP"]  # 0, 1, 2 for group memebership
-  tmpDx1 = tmpLog[,"PRE_10_GENDER"]  # 0, 1, 2 for gender
-  
-  
-  tmp5 = cbind( tmp3, tmpDx, tmpDx1) #Adds column with group membership
-  allData = rbind(allData, tmp5) 
-  if (tmpDx == 0) {
-    if(tmpDx1 == 1){
-      contDataM = rbind(contDataM, tmp5)
-      allM = rbind(allM, tmp5)
-    }
-    if(tmpDx1 ==2){
-      contDataF = rbind(contDataF, tmp5)
-      allF = rbind(allF, tmp5)
-    }
-  }
-  else if (tmpDx == 1) {
-    if(tmpDx1==1){
-      heroinDataM = rbind(heroinDataM, tmp5)
-      allM = rbind(allM, tmp5)
-    }
-    if(tmpDx1==2){
-      heroinDataF = rbind(heroinDataF, tmp5)
-      allF = rbind(allF, tmp5)
-    }
-  } 
-  else if(tmpDx==2){
-    if(tmpDx1==1){
-      amphDataM = rbind(amphDataM, tmp5)
-      allM = rbind(allM, tmp5)
-    }
-    if(tmpDx1==2){
-      amphDataF = rbind(amphDataF, tmp5)
-      allF = rbind(allF, tmp5)
-    }
-  }
-  else if(tmpDx==3){
-    if(tmpDx1==1){
-      polyDataM = rbind(polyDataM, tmp5)
-      allM = rbind(allM, tmp5)
-    }
-    if(tmpDx1==2){
-      polyDataF = rbind(polyDataF, tmp5)
-      allF = rbind(allF, tmp5)
-    }
-  }
-  else if(tmpDx==4){
-    if(tmpDx1==1){
-      sibHeroinM = rbind(sibHeroinM, tmp5)
-      allM = rbind(allM, tmp5)
-    }
-    if(tmpDx1==2){
-      sibHeroinF = rbind(sibHeroinF, tmp5)
-      allF = rbind(allF, tmp5)
-    }
-  }
-  else if(tmpDx==5){
-    if(tmpDx1==1){
-      sibAmphetamineM = rbind(sibAmphetamineM, tmp5)
-      allM = rbind(allM, tmp5)
-    }
-    if (tmpDx1==2){
-      sibAmphetamineF = rbind(sibAmphetamineF, tmp5)
-      allF = rbind(allF, tmp5)
-    }
-  }
+  return(df)
 }
 
-
-#This renames the column titles and removes ' from the choice number
-renameFunction<-function(df){
-  colnames(df)[which(names(df) == "Trial")] <- "trial"
-  colnames(df)[which(names(df) == "Deck")] <- "choice"
-  colnames(df)[which(names(df) == "Win")] <- "gain"
-  colnames(df)[which(names(df) == "Lose")] <- "loss"
-  df$choice <- gsub("'", '', gsub('^,+|,+$', '', df$choice))
+importFunction2<-function(df){
+  df$Lose<-df$Lose*-1 #Makes all the lose values negative
+  df$Deck = gsub("deck1", 1, df$Deck) #Replaces default values to match other database
+  df$Deck = gsub("deck2", 2, df$Deck)
+  df$Deck = gsub("deck3", 3, df$Deck)
+  df$Deck = gsub("deck4", 4, df$Deck)
   return(df)
 }
 
 
+setwd( "C:/Users/shahidir/Dropbox/Research/Data Processing/Raw Data/IGT_Combined" ) #Sets working directory to folder where the IQDAT files are
+allData = NULL #Dataset where all merged IGT data will be placed
+#groupInfo = logData 
+allFiles = dir() #Gets directory of all files
+numSubjs = length( allFiles )
+for (subjIdx in 1:numSubjs) {
+  #subjIdx=326
+  tmpFile = allFiles[ subjIdx ]
+  message(tmpFile)
+  tmp= read.table(tmpFile, skip=21, header=T) #Imports file assuming IGT description is in place
+  
+  if(nrow(tmp)==100){ #If the file contains IGT description, imports this
+    df<-importFunction1(tmp)
+  }
+  if(nrow(tmp)==79){ #If the file does not contain IGT description, imports like this
+    tmp= read.table(tmpFile, header=T,  colClasses=c(date="character", subject="character"))
+    df<-importFunction2(tmp)
+  }
+  if( nrow(tmp)!=100 & nrow(tmp)!=79 ){
+    message(paste("THIS FILE IS CORRUPTED PLEASE CHECK MANUALLY ", tmpFile))
+  }
+  #Renames columns to match requirements for computational modeling
+  colnames(df)[which(names(df) == "Trial")] <- "trial"
+  colnames(df)[which(names(df) == "Deck")] <- "choice"
+  colnames(df)[which(names(df) == "Win")] <- "gain"
+  colnames(df)[which(names(df) == "Lose")] <- "loss"
+  
+  #This is for if there is a master database
+  #tmpLog = subset( groupInfo, PRE_1_SUBJ==subjID)  # find this subject's group membership using PRE_1_SUBJ from SPSS file
+  #tmpDx = tmpLog[,"PRE_GROUP"]  # 0, 1, 2 for group memebership
+  #tmpDx1 = tmpLog[,"PRE_10_GENDER"]  # 0, 1, 2 for gender
+  #df = cbind( df, tmpDx, tmpDx1) #Adds column with group membership
+  
+  #Combines the prepared filesets
+  allData = rbind(allData, df) 
+ 
+}
+
 #Preparing output for all data
-allData<-renameFunction(allData)
 write.table( allData, "allData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for control data
-contDataM<-renameFunction(contDataM)
-contDataF<-renameFunction(contDataF)
-allContData=rbind(contDataM, contDataF)
-write.table( allContData, "contData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( contDataM, "contDataM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( contDataF, "contDataF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for heroin data
-heroinDataM<-renameFunction(heroinDataM)
-heroinDataF<-renameFunction(heroinDataF)
-allHeroinData=rbind(heroinDataM, heroinDataF)
-write.table( allHeroinData, "heroinData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( heroinDataM, "heroinDataM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( heroinDataF, "heroinDataF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for amphetamine data
-amphDataM<-renameFunction(amphDataM)
-amphDataF<-renameFunction(amphDataF)
-allAmphData=rbind(amphDataM, amphDataF)
-write.table( allAmphData, "amphData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( amphDataM, "amphDataM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( amphDataF, "amphDataF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for polysubstance use data
-polyDataM<-renameFunction(polyDataM)
-polyDataF<-renameFunction(polyDataF)
-allPolyData=rbind(polyDataM, polyDataF)
-write.table( allPolyData, "polyData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( polyDataM, "polyDataM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( polyDataF, "polyDataF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for sibling heroin data
-sibHeroinM<-renameFunction(sibHeroinM)
-sibHeroinF<-renameFunction(sibHeroinF)
-allSibHeroinData=rbind(sibHeroinM, sibHeroinF)
-write.table( allSibHeroinData, "sibHeroinData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( sibHeroinM, "sibHeroinDataM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( sibHeroinF, "sibHeroinDataF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for sibling amphetamine data
-sibAmphetamineM<-renameFunction(sibAmphetamineM)
-sibAmphetamineF<-renameFunction(sibAmphetamineF)
-allSibAmphetamineData=rbind(sibAmphetamineM, sibAmphetamineF)
-write.table( allSibAmphetamineData, "sibAmphetamineData.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( sibAmphetamineM, "sibAmphDataM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( sibAmphetamineF, "sibAmphDataF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-
-#Preparing output for sex data
-allM<-renameFunction(allM)
-allF<-renameFunction(allF)
-write.table( allM, "allM.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
-write.table( allF, "allF.txt",row.names=F, col.names=T, sep = "\t", quote=FALSE  )
