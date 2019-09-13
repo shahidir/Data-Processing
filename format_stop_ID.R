5b0af0fc2c70f9cbca867a3d3afc32d07c77b030
plonskipe <54859664+plonskipe@users.noreply.github.com>
2019-09-13 08:55:13 -0400

    Update format_stop_ID.R

--- format_stop_ID.R
+++ format_stop_ID.R
@@ -1,19 +1,45 @@
+# use the online resource managed by Verbruggen at  
+# https://github.com/fredvbrug/STOP-IT/blob/master/Tscope_version/README-ANALYZE-IT-Tscope.md 
+# to process OLD stop data using current best practices
 
+### format ID in individual stop data ###
 rm(list=ls()) # clear the working directory
 
-setwd("C:/Users/Paulie/Desktop/SST/") # set working directory
+setwd("C:/Users/Paulie/Desktop/SST/STOP-IT/") # set working directory
 
-datafile <- "STOP-IT/stop_individual_data.csv" # set datafile name
+datafile <- "stop_individual_data.csv" # set datafile name
 df <- read.csv(datafile) # make data.frame from .csv
 
 ID <- as.vector(df$subject) # create vector of subject column
 
-ID <- gsub("stop-", "", ID) # remove stop- prefix from ID
-ID <- gsub(".txt", "", ID) # remove .txt extension from ID
+ID1 <- gsub("stop-", "", ID) # remove stop- prefix from ID
+ID1 <- gsub(".txt", "", ID1) # remove .txt extension from ID
 
 df2 <- subset(df, select= -c(subject)) # remove subject column
-df <- cbind(ID, df2) # combine new subject column and data
+df <- cbind(ID1, df2) # combine new subject column and data
 
 IDdatafile <- "stopdata.csv" # set new datafile name
 
 write.csv(df, "stopdata.csv" ) # write to csv file
+
+### extract time and add to datafile ###
+
+datafile <- "stopdata.csv" # set datafile name
+data <- read.csv(datafile, header=T) # read data into data.frame
+time <- as.character() # set empty character vector for mtime
+combine <- data.frame() # set empty data.frame
+
+setwd("C:/Users/Paulie/Desktop/SST/STOP-IT/") # set working directory
+
+n = length(ID)
+for(i in 1:n)	
+	{
+	IDfile <- ID[i]
+	subject <- read.table(IDfile)
+	tmp <- as.character(file.info(IDfile)$mtime)
+	time <- rbind(time, tmp)
+	}
+
+combine <- cbind(data, time)
+
+write.csv(combine, "stopdatadate.csv" ) # write to csv file
